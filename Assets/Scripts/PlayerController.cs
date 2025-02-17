94d32267a4bacf93b7fd783b2f7650c5504a83c7
Peter Phan <cryptogameking@protonmail.com>
2025-02-16 23:54:53 -0800

    Completed Unit 1 Lesson

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // class-level variables
+    private Rigidbody rbPlayer;
+    private Vector3 direction = Vector3.zero;
+    [SerializeField]
+    private float forceMultiplier = 1.0f;
+    [SerializeField]
+    private ForceMode forceMode;
+    public GameObject spawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbPlayer = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        // local variables - they're local Update, but not to other methods
+        float horizontalVelocity = Input.GetAxis("Horizontal");
+        float verticalVelocity = Input.GetAxis("Vertical");
+
+        direction = new Vector3(horizontalVelocity, 0, verticalVelocity);
+    }
+
+    // FixedUpdate is called once per frame, along with the Unity's physics engine
+    void FixedUpdate()
+    {
+        rbPlayer.AddForce(direction * forceMultiplier, forceMode); // function/method
+
+        if (transform.position.z > 38)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, 38);
+        }
+        else if (transform.position.z < -38)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -38);
+        }
+    }
+
+    private void Respawn()
+    {
+        rbPlayer.MovePosition(spawnPoint.transform.position);
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.CompareTag("Hazard"))
+        {
+            Respawn();
+        }
+    }
+}
