2a82989496665e423629b48d5e05bc99bd8e8332
Peter Phan <cryptogameking@protonmail.com>
2025-04-20 16:22:25 -0700

    Unit 3 Finished

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -56,6 +56,15 @@ public class PlayerController : NetworkBehaviour
         }
     }
 
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay(transform.position, direction * 10);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, rbPlayer.velocity * 5);
+    }
+
     private void Move(Vector3 input)
     {
         rbPlayer.AddForce(input * forceMultiplier, forceMode); // function/method
@@ -85,6 +94,7 @@ public class PlayerController : NetworkBehaviour
         }
 
         rbPlayer.MovePosition(spawnPoints[index].transform.position);
+        rbPlayer.velocity = Vector3.zero;
     }
 
     void OnTriggerExit(Collider collider)
